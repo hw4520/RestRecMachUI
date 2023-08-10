b5ebc3e655ea92315ff7c7e7d619d78658115d1c
LeeHyeWon <hwlee@okpos.co.kr>
2023-08-10 23:02:39 +0900

    prittier 맞춰서 format 맞추고 버그 잡음

--- src/components/TableComponent/index.js
+++ src/components/TableComponent/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const TableComponent = () => {
   // 테이블 데이터 상태 관리
@@ -8,13 +8,18 @@ const TableComponent = () => {
   const fetchTableData = () => {
     // 샘플 데이터
     const sampleData = [
-      { name: "맛집1", address: "주소1", phone: "전화번호1" },
-      { name: "맛집2", address: "주소2", phone: "전화번호2" },
-      { name: "맛집3", address: "주소3", phone: "전화번호3" },
+      { id: 1, name: "맛집1", address: "주소1", phone: "전화번호1" },
+      { id: 2, name: "맛집2", address: "주소2", phone: "전화번호2" },
+      { id: 3, name: "맛집3", address: "주소3", phone: "전화번호3" },
     ];
 
     setTableData(sampleData);
   };
+
+  // 컴포넌트가 마운트되었을 때 데이터를 가져오도록 useEffect 사용
+  useEffect(() => {
+    fetchTableData();
+  }, []);
 
   // 테이블 컨텐츠 JSX
   const tableContent = (
@@ -28,8 +33,8 @@ const TableComponent = () => {
       </thead>
       <tbody>
         {/* 테이블 데이터를 기반으로 동적으로 행을 생성 */}
-        {tableData.map((row, index) => (
-          <tr key={index}>
+        {tableData.map((row) => (
+          <tr key={row.id}>
             <td>{row.name}</td>
             <td>{row.address}</td>
             <td>{row.phone}</td>
@@ -38,11 +43,7 @@ const TableComponent = () => {
       </tbody>
     </table>
   );
-  return (
-    <div>
-      {tableContent}
-    </div>
-  );
+  return <div>{tableContent}</div>;
 };
 
 export default TableComponent;
--- src/pages/Presentation/sections/Newsletter.js
+++ src/pages/Presentation/sections/Newsletter.js
@@ -13,7 +13,7 @@ Coded by www.creative-tim.com
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
-
+import { useState } from "react";
 // @mui material components
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
@@ -38,36 +38,41 @@ function Newsletter() {
   return (
     <MKBox component="section" pt={6} my={6}>
       <Container>
-      {showTable ? (
-        <TableComponent />
-      ) : (
-        <Grid container alignItems="center">
-          <Grid item sx={12} md={6} sx={{ ml: { xs: 0, lg: 3 }, mb: { xs: 12, md: 0 } }}>
-            <MKTypography variant="h4">맛집 추천</MKTypography>
-            <MKTypography variant="body2" color="text" mb={3}>
-              당신이 선호하는 맛집과 유사한 맛집들을 추천해 드립니다.
-            </MKTypography>
-            <Grid container spacing={1}>
-              <Grid item xs={10}>
-                <MKSelect />
-              </Grid>
-              <Grid item xs={8}>
-                <MKInput type="text" fullWidth placeholder="가게명을 입력해 주세요." />
-              </Grid>
-              <Grid item xs={4}>
-                <MKButton variant="gradient" color="info" sx={{ height: "100%" }} onClick={handleClick}>
-                  검색
-                </MKButton>
+        {showTable ? (
+          <TableComponent />
+        ) : (
+          <Grid container alignItems="center">
+            <Grid item sx={12} md={6} sx={{ ml: { xs: 0, lg: 3 }, mb: { xs: 12, md: 0 } }}>
+              <MKTypography variant="h4">맛집 추천</MKTypography>
+              <MKTypography variant="body2" color="text" mb={3}>
+                당신이 선호하는 맛집과 유사한 맛집들을 추천해 드립니다.
+              </MKTypography>
+              <Grid container spacing={1}>
+                <Grid item xs={10}>
+                  <MKSelect />
+                </Grid>
+                <Grid item xs={8}>
+                  <MKInput type="text" fullWidth placeholder="가게명을 입력해 주세요." />
+                </Grid>
+                <Grid item xs={4}>
+                  <MKButton
+                    variant="gradient"
+                    color="info"
+                    sx={{ height: "100%" }}
+                    onClick={handleClick}
+                  >
+                    검색
+                  </MKButton>
+                </Grid>
               </Grid>
             </Grid>
+            <Grid item xs={12} md={5} sx={{ ml: "auto" }}>
+              <MKBox position="relative">
+                <MKBox component="img" src={macbook} alt="macbook" width="100%" />
+              </MKBox>
+            </Grid>
           </Grid>
-          <Grid item xs={12} md={5} sx={{ ml: "auto" }}>
-            <MKBox position="relative">
-              <MKBox component="img" src={macbook} alt="macbook" width="100%" />
-            </MKBox>
-          </Grid>
-        </Grid>
-         )}
+        )}
       </Container>
     </MKBox>
   );
